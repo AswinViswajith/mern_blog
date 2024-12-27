e4562eb66081463e06bc3931bb134387ca0824bb
Aswin Viswajith N R <103254420+AswinViswajith@users.noreply.github.com>
2024-12-27 15:35:20 +0530

    Add files via upload

---
+++ client/src/pages/About.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">About</h1>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex sed adipisci
+        tempore, suscipit aliquam at nisi nesciunt provident aperiam dolorem
+        possimus facere officia quisquam voluptate cupiditate repellendus
+        explicabo nihil quo itaque deleniti fugiat? Eum sint reiciendis in earum
+        ipsa a consequatur temporibus, nulla expedita veniam sequi! Dignissimos
+        eum facilis magni.
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex sed adipisci
+        tempore, suscipit aliquam at nisi nesciunt provident aperiam dolorem
+        possimus facere officia quisquam voluptate cupiditate repellendus
+        explicabo nihil quo itaque deleniti fugiat? Eum sint reiciendis in earum
+        ipsa a consequatur temporibus, nulla expedita veniam sequi! Dignissimos
+        eum facilis magni.
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+        deleniti necessitatibus dolores nihil doloremque soluta eum est a
+        eveniet corporis ipsam dicta ex, modi doloribus ipsa amet corrupti.
+        Veniam pariatur nihil dolores sapiente explicabo excepturi fugiat fugit
+        cum recusandae architecto et quas, vero consequuntur ducimus? Fuga,
+        maxime expedita nostrum at nesciunt mollitia debitis doloremque ipsa
+        sequi facilis ex quaerat ipsam perspiciatis sit quia consectetur nihil
+        culpa deserunt atque quidem, voluptas non voluptatibus. Minus distinctio
+        porro perferendis doloribus quos aspernatur laudantium. Tenetur laborum
+        atque quis obcaecati ut rem repudiandae aut, amet quas exercitationem
+        harum perferendis molestiae, iusto repellendus voluptatum dolore neque.
+      </p>
+    </div>
+  )
+}
+
+export default About
